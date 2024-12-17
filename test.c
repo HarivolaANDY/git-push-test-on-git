3d2ef6d7869ba882097362a7585df54762c5dc2c
HarivolaAndy <hei.andy.43@gmail.com>
2024-12-17 15:11:22 +0300

    refactor: create an addition function to modularize the code

--- test.c
+++ test.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+int addition(int x, int y) {
+    return x + y;
+}
+
 int main() {
-    int a = 5, b = 10;
-    int c = a + b;
-    printf("c = %d\n", c);
+    int a = 5, b = 10, resultat;
+
+    resultat = addition(a, b);
+
+    printf("La somme de %d et %d est : %d\n", a, b, resultat);
+
     return 0;
 }
